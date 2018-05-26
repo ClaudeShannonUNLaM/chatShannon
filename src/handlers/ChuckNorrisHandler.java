4ad151e93baac5e80436e154f3264289140ae6be
Marino Lautaro <lautaro.marino@engee.com.ar>
2018-05-26 20:42:39 -0300

    GRAL: Se suben los handlers del ejercicio 5 y 13 y modificaciones al conversor.

---
+++ src/handlers/ChuckNorrisHandler.java
@@ -0,0 +1,11 @@
+package handlers;
+
+public class ChuckNorrisHandler extends AsistantSentenceHandler {
+
+	@Override
+	public String giveAnswer(String mensaje, String nombreUsuario) {
+		
+		return null;
+	}
+
+}
---
+++ src/handlers/TriviaHandler.java
@@ -0,0 +1,12 @@
+package handlers;
+
+public class TriviaHandler extends AsistantSentenceHandler {
+
+	@Override
+	public String giveAnswer(String mensaje, String nombreUsuario) {
+	
+		return null;
+	}
+
+	
+}
